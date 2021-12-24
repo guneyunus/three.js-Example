8ecb51d30f27f8f6b119af3a6513d6c28fcf9831
guneyunus <guney.yunus.dev@outlook.com>
2021-12-24 05:19:59 +0300

    three.js objec3d class

--- transform-object/src/script.js
+++ transform-object/src/script.js
@@ -1,31 +1,39 @@
 import './style.css'
 import * as THREE from 'three'
 
+// Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Sizes
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+mesh.position.y = 1
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: 800,
     height: 600
 }
 
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({
-    color: '#ff0000'
-})
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-scene.add(cubeMesh)
-
-// Camera
+/**
+ * Camera
+ */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
